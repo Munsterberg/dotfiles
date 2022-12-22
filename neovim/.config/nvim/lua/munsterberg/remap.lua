8d1de43131c71dfd048d21db49dd0851c31e7e94
Jake <jetrussell93@gmail.com>
2022-12-22 17:38:07 -0500

    updates

--- neovim/.config/nvim/lua/munsterberg/remap.lua
+++ neovim/.config/nvim/lua/munsterberg/remap.lua
@@ -21,6 +21,10 @@ vim.keymap.set("n", "<leader>bl", ":blast<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<C-j>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-k>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-l>", "<C-W><C-L>")
+vim.keymap.set("n", "<C-h>", "<C-W><C-H>")
 
 vim.keymap.set("n", "<leader>fa", ":Format<CR>")
 
